c82bb9930f4584cc208b88a83b6dce745915129f
amogh-navana <amogh@navanatech.in>
2021-02-12 15:46:03 +0530

    add permissions to manifest and request them in onCreate
    Signed-off-by: amogh-navana <amogh@navanatech.in>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.example.myapplication
 
+import android.Manifest
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setWebView()
 
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                Manifest.permission.RECORD_AUDIO
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                    if (p0?.areAllPermissionsGranted() == true) {
+                        setWebView()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+//                    TODO("Not yet implemented")
+                }
+
+            })
     }
 
     private fun setWebView() {
@@ -20,7 +45,7 @@ class MainActivity : AppCompatActivity() {
             WebView.setWebContentsDebuggingEnabled(true)
         }
         webView.settings.javaScriptEnabled = true;
-        webView.addJavascriptInterface( WebAppInterface(this),"firstinterface");
+        webView.addJavascriptInterface(WebAppInterface(this), "firstinterface");
         webView.loadUrl("file:///android_asset/index.html")
 
     }
